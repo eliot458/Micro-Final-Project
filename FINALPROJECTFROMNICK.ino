77ee1641f0b46009aaeea791c62e260807601edb
eliot458 <63027017+eliot458@users.noreply.github.com>
2020-04-03 13:52:29 -0400

    Update FINALPROJECTFROMNICK.ino

--- FINALPROJECTFROMNICK.ino
+++ FINALPROJECTFROMNICK.ino
@@ -2,35 +2,34 @@
 // include the library code:
 #include <LiquidCrystal.h>
 #include <math.h>
-const int ledPin = 13;
-const int buttonPin = 25;
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-const int buzzer = 6;
-const int buttonGreen = 7;
-const int ledGreen = 30;
-const int buttonWhite = 10;
-const int ledWhite = 26;
-const int buttonRed = 9;
-const int ledRed = 32;
-const int buttonBlue = 8;
-const int ledBlue = 28;
 
-long lightNum;
+const int buttonPin = 25; // start button pin
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; // LCD pins
+const int buzzer = 6; // buzzer pin
+const int buttonGreen = 7; // green button pin
+const int ledGreen = 30; // green LED pin
+const int buttonWhite = 10; // white button pin
+const int ledWhite = 26; // white LED pin
+const int buttonRed = 9; // red button pin
+const int ledRed = 32; // red LED pin
+const int buttonBlue = 8; // blue button pin
+const int ledBlue = 28; // blue LED pin
 
-int timer = 9;
-int buttonState = 0;
-int State = 0;
-int buttonNew;
-int buttonOld = 1;
+long lightNum; // integer associated with each light color
 
 
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+int buttonState = 0; // makes initial buttonstate 0
+int State = 0; // makes initial state off, if state 1, game is running
+int buttonNew; // value to make the toggle start/stop button
+int buttonOld = 1; // value to make the toggle start/stop button
+
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7); // setup pins for LCD
 
 
 void setup() {
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT); // Creates inputs and outputs for the various LED/Buttons
   pinMode(ledPin, OUTPUT);
-  Serial.begin(9600); // Starts the serial communication
   pinMode(buttonGreen, INPUT);
   pinMode(ledGreen, OUTPUT);
   pinMode(buttonWhite, INPUT);
@@ -40,7 +39,8 @@ void setup() {
   pinMode(buttonBlue, INPUT);
   pinMode(ledBlue, OUTPUT);
 
-  randomSeed(analogRead(0));
+  Serial.begin(9600); // Starts the serial communication
+  randomSeed(analogRead(0)); // reads static voltage of the air to determine a random seed value of the random number generator
 
 }
 
@@ -49,12 +49,12 @@ void loop() {
 
   buttonState = digitalRead(buttonPin);
 
-  int greenStatus = digitalRead(buttonGreen);
-  int whiteStatus = digitalRead(buttonWhite);
-  int redStatus = digitalRead(buttonRed);
-  int blueStatus = digitalRead(buttonBlue);
+  int greenStatus = digitalRead(buttonGreen); // makes greenstatus equivalent to green button
+  int whiteStatus = digitalRead(buttonWhite); // makes white status equivalent to white button
+  int redStatus = digitalRead(buttonRed); // makes redstatus equivalent to red button
+  int blueStatus = digitalRead(buttonBlue); // makes bluestatus equivalent to blue button
 
-  Serial.print(greenStatus);
+  Serial.print(greenStatus); // serial prints the button values
   Serial.println();
   Serial.print(whiteStatus);
   Serial.println();
@@ -63,45 +63,46 @@ void loop() {
   Serial.print(blueStatus);
   Serial.println();
 
-  timer = 9000;
-  int level = 1;
-  lcd.setCursor(0, 1);
-  lcd.clear();
-  buttonNew = digitalRead(buttonPin);
-  if (buttonOld == 0 && buttonNew == 1)
+  int timer = 9000;  // creates a timer value of 9000
+  int level = 1; // starts the player on "level 1"
+  lcd.clear(); // clears the led of anything that could be on it
+  buttonNew = digitalRead(buttonPin); // sets the new button as value of start button
+  if (buttonOld == 0 && buttonNew == 1) // turns on the game if it was off before
   {
     if (State == 0)
     {
-      State = 1;
+      State = 1; // if the start button has been pressed, the game will turn on
 
       while (State == 1)
 
 
-      { for (timer > 0; (timer = timer - level);)
-        { lcd.begin(16, 2);
-          lcd.print("Level: " && level);
-          lightNum = random(1, 5);
-          Serial.print(lightNum);
+      { for (timer > 0; (timer = timer - level);) // loop, if the timer is above 0, the timer will subtract by the integer amount of what level the player is on
+        { lcd.begin(16, 2); // LCD begin writing on top line
+          lcd.print("Level: " && level); // writes what level the player is on
+          lightNum = random(1, 5); // creates a random integer that corresponds to which light will turn on
+          Serial.print(lightNum); // serial monitor will print which value the random number generator creates
           {
-            tone(buzzer, 500, 100);
+            tone(buzzer, 500, 100); // makes a tone 
           }
-          for (lightNum == 1; (timer = timer - level);)
+          for (lightNum == 1; (timer = timer - level);) // if the random integer is 1, this loop will happen. causes timer to decrease by whichever level you are on
+          //for example level 1; timer - 1, level 2; timer - 2, etc.
           {
 
 
-            digitalWrite(ledGreen, HIGH);
-            digitalWrite(ledWhite, LOW);
+            digitalWrite(ledGreen, HIGH); // determines green light will turn on
+            digitalWrite(ledWhite, LOW); // all other lights will turn off
             digitalWrite(ledRed, LOW);
             digitalWrite(ledBlue, LOW);
-            lcd.setCursor(0, 1);
-            lcd.print(timer);
+            lcd.setCursor(0, 1); // sets cursor to bottom line
+            lcd.print(timer); // prints the value of timer
 
-            if ((timer == 1000) || (timer == 2000) || (timer == 3000) || (timer == 4000) || (timer == 5000) || (timer == 6000) || (timer == 7000) || (timer == 8000) || (timer == 8998))
+            if ((timer == 1000) || (timer == 2000) || (timer == 3000) || (timer == 4000) // createas a tone at each interval of 1000 from the buzzer
+                || (timer == 5000) || (timer == 6000) || (timer == 7000) || (timer == 8000) || (timer == 8998))
             {
-              tone(buzzer, 500, 100);
+              tone(buzzer, 500, 100); // buzzer tone is 500hz and lasts for 100 milliseconds
             }
 
-            int greenStatus = digitalRead(buttonGreen);
+            int greenStatus = digitalRead(buttonGreen); // reads each of the LED pins
             int whiteStatus = digitalRead(buttonWhite);
             int redStatus = digitalRead(buttonRed);
             int blueStatus = digitalRead(buttonBlue);
@@ -109,45 +110,52 @@ void loop() {
             Serial.print(greenStatus);
             Serial.println();
 
-            if (greenStatus >= 1)
+            if (greenStatus >= 1) // if played presses the button associated with the green light, this will become true
             {
-              level++;
-              timer = 9000;
-              Serial.print(level);
-              break;
+              level++; // the level value will increase
+              timer = 9000; // the timer will be reset
+              Serial.print(level); // the serial monitor will print which level the player is now on
+              break; // break the loop to go back and create a new random light number
             }
-            if ((whiteStatus == 1) || (redStatus == 1) || (blueStatus == 1))
+            if ((whiteStatus == 1) || (redStatus == 1) || (blueStatus == 1)) // if the player presses the button associated with any of the other lights,
+              // these values will go high
             {
-              lcd.setCursor(0, 1);
-              lcd.print("You Lose");
+              lcd.setCursor(0, 1); // set LCD curson to bottom line
+              lcd.print("You Lose"); // prints you lose on LCD
 
-              tone(buzzer, 100, 1000);
-              digitalWrite(ledRed, LOW);
+              tone(buzzer, 100, 1000); // Creates a buzz for 100 hz for 1000 milliseconds
+              digitalWrite(ledRed, LOW); 
               delay(1000);
-              noTone(buzzer);
+              noTone(buzzer); // turns off the buzzer
               delay(5000);
-              State = 0;
-              return 0;
+              State = 0; // resets the game to be turned off
+              return 0; // returns to the top of the program
 
             }
-            if (timer <= 0)
+            if (timer <= 0) // if the timer reaches 0
             {
               {
-                timer = 0;
-                level = 0;
+                timer = 0; 
+                level = 0; // means player loses
 
 
-                lcd.setCursor(0, 1);
-                lcd.print("You Lose");
+                lcd.setCursor(0, 1); // sets lcd cursor bottom line
+                lcd.print("You Lose"); // prints you lose on LCD
 
-                tone(buzzer, 100, 1000);
+                tone(buzzer, 100, 1000); // Creates a buzz for 100 hz for 1000 milliseconds
                 digitalWrite(ledRed, LOW);
                 delay(1000);
                 noTone(buzzer);
                 delay(5000);
-                State = 0;
+                State = 0; // puts player out of the game
                 return 0;
               }
+              
+       // THIS IS THE END OF THE FIRST SECTION, ALL SECTIONS AFTER THIS ARE REPEATS WITH DIFFERENT LEDS AND BUTTONS
+              
+              
+              
+              
             }
             for (lightNum == 2; (timer = timer - level);)
             {
@@ -327,6 +335,6 @@ void loop() {
       }
     }
   }
-  buttonOld = buttonNew;
-  delay(100);
+  buttonOld = buttonNew; // used to check if the start button has been pressed again
+  delay(100); // delay to get rid of bounce
 }
